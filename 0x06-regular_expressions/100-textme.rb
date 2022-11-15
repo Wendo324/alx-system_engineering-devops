fc148793530b5be2c202f783c2fc74c9c54e0c19
Wendo324 <githakawendo@students.uonbi.ac.ke>
2022-11-15 18:51:37 +0300

    task1

--- 0x06-regular_expressions/100-textme.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-ptn = /(?:(?<=from:)(?:\p{L}+|\+?\d+)|(?<=to:)(?:\p{L}+|\+?\d*)|(?<=flags:)(?:-?\d+:?)*)/
-puts ARGV[0].scan(ptn).join(',')
--- 0x06-regular_expressions/6-phone_number.rb
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env ruby
-puts ARGV[0].scan(/^[1-9]\d{9}$/).join
