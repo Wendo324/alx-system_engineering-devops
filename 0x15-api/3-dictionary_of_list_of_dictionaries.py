b02723dc53cb783817bd5353dd9587bebd4b16bc
Wendo324 <githakawendo@students.uonbi.ac.ke>
2023-02-08 20:29:18 +0300

    task

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,48 +1,33 @@
 #!/usr/bin/python3
 """
-Gather data about employees TODO and export to JSON
+Using https://jsonplaceholder.typicode.com
+gathers data from API and exports it to JSON file
+Implemented using recursion
 """
 import json
 import requests
-import sys
-
-users_url = "https://jsonplaceholder.typicode.com/users?id="
-todos_url = "https://jsonplaceholder.typicode.com/todos"
 
 
-def user_info():
-    """ Fetch user info """
-    
-    from collections import defaultdict
-    correct_output = defaultdict(list)
+API = "https://jsonplaceholder.typicode.com"
+"""REST API url"""
 
-    response = requests.get(todos_url).json()
-    for item in response:
-        url = users_url + str(item['userId'])
-        usr_resp = requests.get(url).json()
-        correct_output[item['userId']].append(
-            {'username': usr_resp[0]['username'],
-            'completed': item['completed'],
-            'task': item['title']})
 
-    with open('todo_all_employees.json', 'r') as f:
-        student_output = json.load(f)
-
-    error = False
-    for correct_key, correct_entry in correct_output.items():
-        flag = 0
-        for student_key, student_entry in student_output.items():
-            if str(correct_key) == str(student_key):
-                flag = 1
-                if correct_entry != student_entry:
-                    print("User ID {} Tasks: Incorrect".format(str(correct_key)))
-                    error = True
-        if flag == 0:
-            print("User ID {}: Not found".format(str(correct_key)))
-            error = True
-
-    if not error:
-        print("User ID and Tasks output: OK")
-
-if __name__ == "__main__":
-    user_info()
+if __name__ == '__main__':
+    users_res = requests.get('{}/users'.format(API)).json()
+    todos_res = requests.get('{}/todos'.format(API)).json()
+    users_data = {}
+    for user in users_res:
+        id = user.get('id')
+        user_name = user.get('username')
+        todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+        user_data = list(map(
+            lambda x: {
+                'username': user_name,
+                'task': x.get('title'),
+                'completed': x.get('completed')
+            },
+            todos
+        ))
+        users_data['{}'.format(id)] = user_data
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(users_data, file)
