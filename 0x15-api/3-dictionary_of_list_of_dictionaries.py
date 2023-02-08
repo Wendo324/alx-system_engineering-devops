dbeb503631964879629cc8f6c03d5b7b9fd46f8e
Wendo324 <githakawendo@students.uonbi.ac.ke>
2023-02-08 20:26:56 +0300

    task

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,48 @@
 #!/usr/bin/python3
-"""Exports to-do list information of all employees to JSON format."""
+"""
+Gather data about employees TODO and export to JSON
+"""
 import json
 import requests
+import sys
+
+users_url = "https://jsonplaceholder.typicode.com/users?id="
+todos_url = "https://jsonplaceholder.typicode.com/todos"
+
+
+def user_info():
+    """ Fetch user info """
+    
+    from collections import defaultdict
+    correct_output = defaultdict(list)
+
+    response = requests.get(todos_url).json()
+    for item in response:
+        url = users_url + str(item['userId'])
+        usr_resp = requests.get(url).json()
+        correct_output[item['userId']].append(
+            {'username': usr_resp[0]['username'],
+            'completed': item['completed'],
+            'task': item['title']})
+
+    with open('todo_all_employees.json', 'r') as f:
+        student_output = json.load(f)
+
+    error = False
+    for correct_key, correct_entry in correct_output.items():
+        flag = 0
+        for student_key, student_entry in student_output.items():
+            if str(correct_key) == str(student_key):
+                flag = 1
+                if correct_entry != student_entry:
+                    print("User ID {} Tasks: Incorrect".format(str(correct_key)))
+                    error = True
+        if flag == 0:
+            print("User ID {}: Not found".format(str(correct_key)))
+            error = True
+
+    if not error:
+        print("User ID and Tasks output: OK")
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
-
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+    user_info()
